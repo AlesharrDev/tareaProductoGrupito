501e0c0dde22e6e699d5f8440f49b73705a55136
Nafta <161855796+wilmanProyect@users.noreply.github.com>
2025-03-05 22:33:28 -0400

    list

--- app/(stack)/home/index.tsx
+++ app/(stack)/home/index.tsx
@@ -1,21 +1,49 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
-import globalStyle from '@/app/style/globalstyle'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import globalStyle from '@/app/style/globalstyle';
 
 const Home = () => {
-  
+  const [nombre, setNombre] = useState('');
+  const [precio, setPrecio] = useState('');
+  const [stock, setStock] = useState('');
+
+  const handleCreate = () => {
+    console.log('Producto creado:', { nombre, precio, stock });
+
+  };
+
   return (
     <View style={globalStyle.container}>
-      <Text style={globalStyle.h1}>Intentacio de producto</Text>
-      <TextInput style={globalStyle.input} placeholder="Nombre del producto" />
-      <TextInput style={globalStyle.input} placeholder="Precio" />
-      <TextInput style={globalStyle.input} placeholder="Stock" />
-      <TouchableOpacity style={globalStyle.button}>
+      <Text style={globalStyle.h1}>Intención de producto</Text>
+      
+      <TextInput
+        style={globalStyle.input}
+        placeholder="Nombre del producto"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+      
+      <TextInput
+        style={globalStyle.input}
+        placeholder="Precio"
+        value={precio}
+        onChangeText={setPrecio}
+        keyboardType="numeric"
+      />
+      
+      <TextInput
+        style={globalStyle.input}
+        placeholder="Stock"
+        value={stock}
+        onChangeText={setStock}
+        keyboardType="numeric"
+      />
+      
+      <TouchableOpacity style={globalStyle.button} onPress={handleCreate}>
         <Text style={globalStyle.buttonText}>Crear</Text>
       </TouchableOpacity>
     </View>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
